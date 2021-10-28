cc1ff600b26868cdff271ad401b5807e902612e7
Angel Javier <angel_java@hotmail.com>
2021-10-28 09:28:57 -0500

    Movie controller and EntityFramework was added to project

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        [Required, StringLength(maximumLength: 30, MinimumLength = 3)]
+        public string Title { get; set; }
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "yyyy-MM-dd")]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Range(1, 1000), Required]
+        public decimal Price { get; set; }
+    }
+}
